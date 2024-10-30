fb3187d960ae7662343af066a5c4ca7c38fa616e
Magd13 <joelmagdielriveraarce@gmail.com>
2024-10-29 22:25:37 -0500

    Inicio de proyecto

---
+++ src/main/java/alarm/community/panicbutton/PanicButtonAppApplication.java
@@ -0,0 +1,13 @@
+package alarm.community.panicbutton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanicButtonAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanicButtonAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alarm/community/panicbutton/PanicButtonAppApplicationTests.java
@@ -0,0 +1,13 @@
+package alarm.community.panicbutton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanicButtonAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
